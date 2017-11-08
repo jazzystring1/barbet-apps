e10fc39e4050dcbd3f61eca1582c88b0c91d1f72
jazzystring1 <33471627+jazzystring1@users.noreply.github.com>
2017-11-08 09:52:37 +0800

    Add files via upload

---
+++ public/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="index.php" method="post">
+		<input type="text" name="username">
+		<input type="submit" name="submit">
+
+	</form>
+</body>
+</html>
